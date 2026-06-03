fee95dd6a97b8d94e3d7421225b5d6d0ada32c2a
Sinryou <li_yunghsing@outlook.com>
2026-06-03 22:57:44 +0800

    迁移到PerMonitorV2以支持多屏幕DPI更新

--- ImgsToPDF/Program.cs
+++ ImgsToPDF/Program.cs
@@ -5,26 +5,24 @@ using System.Windows.Forms;
 
 namespace ImgsToPDF {
     internal static class Program {
-        [DllImport("User32.dll", SetLastError = false, ExactSpelling = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool SetProcessDPIAware();
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main() {
-            Mutex obj = new(true, "Global\\ImgsToPDF", out bool isFirstInstance);
-            if (isFirstInstance) {  // 仅保留第一个窗口实例 新建跳过
-#if NETCOREAPP3_0_OR_GREATER
-                Application.SetHighDpiMode(HighDpiMode.SystemAware);
-#else
-                SetProcessDPIAware();   // 调用User32.dll中的WinAPI来解决Winform的DPI问题
-#endif
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new ImgsToPDF());
-                GC.KeepAlive(obj);
+            using Mutex mutex = new(
+                true,
+                @"ImgsToPDF",
+                out bool isFirstInstance);
+
+            if (!isFirstInstance) {
+                return;
             }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new ImgsToPDF());
         }
     }
 }
